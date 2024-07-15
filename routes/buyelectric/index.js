396666ade73d66f914a07dc102ab71ce1b781d85
gloriouspete <peter4@email.com>
2024-07-15 06:45:38 +0100

    dee

--- routes/buyelectric/index.js
+++ routes/buyelectric/index.js
@@ -1,16 +1,19 @@
 const executor = require("../../config/db.js");
 require("dotenv").config();
-const mydate = new Date()
+const mydate = new Date();
 const apiKey = process.env.VT_SAND_API;
 const secretKey = process.env.VT_SAND_SECRET;
 const axios = require("axios");
-const Gettime = require("../../services/getTime.js")
+const Gettime = require("../../services/getTime.js");
 async function Buyelectric(req, res) {
   const { userid } = req.user;
   const { billersCode, serviceID, variation_code, phone, amount } = req.body;
+  const datas = { billersCode, serviceID, variation_code, phone, amount };
+  console.log(datas, "lol");
   const intamount = parseInt(amount, 10);
 
   try {
+    const requesttime = Gettime();
     const [userData] = await executor(
       "SELECT credit FROM users WHERE userid = ?",
       [userid]
@@ -31,72 +34,75 @@ async function Buyelectric(req, res) {
         success: false,
       });
     } else if (balance >= intamount) {
-      let data = JSON.stringify({
-        request_id:`${Gettime()}erf`,
-        billersCode: 1111111111111,
-        serviceID: serviceID,
-        variation_code: variation_code,
-        phone: phone,
-        amount: amount,
-      });
-
-      const response = await axios.post(
-        `https://sandbox.vtpass.com/api/pay`,
-        data,
-        {
-          headers: {
+  
+    let data = {
+      request_id: `${requesttime}erf`,
+      billersCode: 1111111111111,
+      serviceID: serviceID,
+      variation_code: variation_code,
+      phone: phone,
+      amount: amount,
+    };
+    console.warn(data, "uggh");
+    const response = await axios.post(
+      `https://sandbox.vtpass.com/api/pay`,
+      data,
+      {
+        headers: {
           "api-key": apiKey,
           "secret-key": secretKey,
-          },
-        }
-      );
-      const responseData = response.data;
-      console.log(responseData);
-
-      if (response.status === 200) {
-        const {
-          content: {
-            transactions: {
-              unique_element,
-              phone,
-              product_name
-            }
-          },
-          token,
-          purchased_code,
-          units
-        } = responseData;
-
-        const imade = {
-          userid,
-          network:"Electricity",
-          recipient:unique_element,
-          Status: "successful",
-          name:product_name,
-          token: token || purchased_code,
-          plan:units,
-          amount,
-        };
-
-        await setElectric(imade);
-        await executor(
-          "UPDATE users SET credit = credit - ? WHERE userid = ?",
-          [intamount, userid]
-        );
-        return res.status(200).json({
-          message: `Your Electric Purchase Transaction was Successful and the token is ${token}`,
-          success: true,
-        });
-      } else {
-        return res.status(500).json({
-          message: "Electricity Purchase Failed, Kindly Try Again later ",
-          success: false,
-        });
+        },
       }
+    );
+    const responseData = response.data;
+    console.log(responseData);
+
+    if (responseData.code === "000") {
+      const {
+        content: {
+          transactions: { unique_element, phone, product_name },
+        },
+        token,
+        purchased_code,
+        units,
+      } = responseData;
+
+      const imade = {
+        userid,
+        network: "Electricity",
+        recipient: unique_element,
+        Status: "successful",
+        name: product_name,
+        token: token || purchased_code,
+        plan: units,
+        amount,
+      };
+
+      await setElectric(imade);
+      await executor("UPDATE users SET credit = credit - ? WHERE userid = ?", [
+        intamount,
+        userid,
+      ]);
+      return res.status(200).json({
+        message: `Your Electric Purchase Transaction was Successful and the token is ${token}`,
+        success: true,
+      });
+    } else if (responseData.code === "099") {
+      return res.status(500).json({
+        message: `Electricity Purchase is processing, Kindly contact support with the code ${requesttime} `,
+        success: true,
+      });
+    } else {
+      return res.status(500).json({
+        message: "Electricity Purchase Failed, Kindly Try Again later ",
+        success: false,
+      });
     }
+  }
   } catch (error) {
     const responsed = {
-      message:"We apologize, we are currently unable to process your electricity plan purchase. Please try again later.",
+      message:
+        "We apologize, we are currently unable to process your electricity plan purchase. Please try again later.",
       success: false,
       data: error,
     };
@@ -106,19 +112,11 @@ async function Buyelectric(req, res) {
 }
 
 const setElectric = async (data) => {
-  const {
-    userid,
-    token,
-    recipient,
-    Status,
-    network,
-    plan,
-    amount,
-    name
-  } = data;
+  const { userid, token, recipient, Status, network, plan, amount, name } =
+    data;
 
   const newDate = new Date();
-  const formattedDate = newDate.toISOString()
+  const formattedDate = newDate.toISOString();
   try {
     const query = `INSERT INTO transactions (userid,recipient, name, status, price, date, network, token,plan,service) VALUES (?,?,?,?,?,?,?,?,?,?)`;
     executor(query, [
@@ -134,14 +132,12 @@ const setElectric = async (data) => {
       "electric",
     ])
       .then((results) => {
-         console.log("successful!", results);
+        console.log("successful!", results);
       })
       .catch((error) => {
-         console.log("error setting transaction",mydate);
+        console.log("error setting transaction", mydate);
       });
-  } catch (error) {
-  
-  }
+  } catch (error) {}
 };
 
 module.exports = Buyelectric;
