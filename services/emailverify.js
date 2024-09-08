8a969a1762245aa54286c0f275b0f54895811ca5
gloriouspete <peter4@email.com>
2024-09-08 14:08:10 +0100

    dee

--- services/emailverify.js
+++ services/emailverify.js
@@ -4,14 +4,14 @@ const Vemail = async (userid) => {
     try {
         const query = `SELECT emailverified FROM users WHERE userid = ?`;
         const result = await executor(query, [userid]);
-
         if (result.length > 0) {
+            console.error(result)
             // Ensure that result is an array with at least one item
             const emailVerifiedStatus = result[0].emailverified;
-            return emailVerifiedStatus;
+            return emailVerifiedStatus === 'yes' ? 'yes' : 'no';
         } else {
-       
-            return null; 
+            
+            return 'no'; 
         }
     } catch (error) {
         console.error("Error retrieving email verification status:", error);
