48706a40e6fc880d939f7d3ac4cd1a3a4beedb7d
gloriouspete <peterninyo4@gmail.com>
2024-11-10 13:37:02 +0100

    dee

--- config/db.js
+++ config/db.js
@@ -3,17 +3,17 @@ const Executor = require ('sqlexecutor');
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD
 
-// const executor = new Executor({
-//     host:"localhost",
-//     user:"apollomo_jaan",
-//     password:password,
-//     database:"apollomo_jaan",
-// }).call
 const executor = new Executor({
     host:"localhost",
-    user:"root",
-    password:"",
-    database:"sys",
+    user:"apollomo_jaan",
+    password:password,
+    database:"apollomo_jaan",
 }).call
+// const executor = new Executor({
+//     host:"localhost",
+//     user:"root",
+//     password:"",
+//     database:"sys",
+// }).call
 
 module.exports = executor;
