aab0072f9e73ed30145371efeccc1562c692f5e5
gloriouspete <peterninyo4@gmail.com>
2025-02-12 05:20:35 +0100

    dee

--- vendor/getdata/getdata.js
+++ vendor/getdata/getdata.js
@@ -62,7 +62,7 @@ async function GetVenddata(req, res) {
 module.exports = GetVenddata;
 function transformData(data, type) {
     return data
-        .filter(product => product.plan_type !== 'DATA COUPONS' && product.plan_type === type)
+        .filter(product => product.plan_type !== 'DATA COUPONS' && product.plan_type === type.toUpperCase())
         .map(product => {
             const { plan_amount, plan_network, id, plan, month_validate, plan_type } = product;
             const multipliedAmount = parseFloat(plan_amount) * 1.03;
