d27a162a47fc3aa1cd47aa38b5b215264492baa8
gloriouspete <peterninyo4@gmail.com>
2025-02-25 10:35:37 +0100

    fdd

--- routes/signup/signup.js
+++ routes/signup/signup.js
@@ -7,6 +7,18 @@ const secretKey = process.env.SECRET;
 async function Signup(req, res) {
   const { name, username, phone, password, email, referrer } = req.body;
   try {
+    function isValidEmail(email) {
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      return emailRegex.test(email);
+    }
+    if (!isValidEmail(email)) {
+      return res.status(400).json({
+        success: false,
+        message: "Email is in incorrect format, Recheck it please",
+        data: null,
+      });
+    }
+    
     // Check if the phone number already exists
     const phoneQuery =
       "SELECT COUNT(*) AS phoneCount FROM users WHERE phone = ?";
